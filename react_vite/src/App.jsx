6d5515ce0bdf7354f08b926dfb42d62de1666ad6
rafan4535 <rafan4535@gmail.com>
2023-03-11 23:27:25 +0600

    start

---
+++ react_vite/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="App">
+      <ol>
+        <li>one</li>
+        <li>two</li>
+        <li>three</li>
+        <li>four</li>
+        <li>five</li>
+      </ol>
+    </div>
+  )
+}
